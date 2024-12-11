07df60a0f62bedf5472a7c6d8cac70d5c461b4ce
Miloš Ančevski <miki2000milos@gmail.com>
2024-12-11 03:15:50 +0100

    fykmx

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -50,41 +50,5 @@ describe('App Component Tests', () => {
       // Check for the "Log Out" button
       expect(logoutButton).toBeInTheDocument();
     });
-
-    test('renders reservations list when View All Reservations is clicked (for admin)', async () => {
-      render(<App />);
-
-      // Simulate login
-      fireEvent.change(screen.getByPlaceholderText(/Email/i), { target: { value: 'admin@a.com' } });
-      fireEvent.change(screen.getByPlaceholderText(/Password/i), { target: { value: '123123' } });
-      fireEvent.click(screen.getByRole('button', { name: /Login/i }));
-
-      // Wait for the "Reserve" button to appear
-      const reserveButton = await waitFor(() => screen.getByRole('button', { name: /View All Reservations/i }));
-
-      fireEvent.click(reserveButton);
-
-      // Check for reservation form
-      const listAllVehicles = await waitFor(() => screen.getByText(/Current Car Reservations/i));
-      expect(listAllVehicles).toBeInTheDocument();
-    });
-
-    test('renders vehicle list when Reserve Vehicle is clicked (for driver)', async () => {
-      render(<App />);
-
-      // Simulate login
-      fireEvent.change(screen.getByPlaceholderText(/Email/i), { target: { value: 'driver@a.com' } });
-      fireEvent.change(screen.getByPlaceholderText(/Password/i), { target: { value: '123123' } });
-      fireEvent.click(screen.getByRole('button', { name: /Login/i }));
-
-      // Wait for the "Reserve" button to appear
-      const reserveButton = await waitFor(() => screen.getByRole('button', { name: /Reserve Vehicle/i }));
-
-      fireEvent.click(reserveButton);
-
-      // Check for reservation form
-      const listAllVehicles = await waitFor(() => screen.getByText(/List of all vehicles/i));
-      expect(listAllVehicles).toBeInTheDocument();
-    });
   });
 });
